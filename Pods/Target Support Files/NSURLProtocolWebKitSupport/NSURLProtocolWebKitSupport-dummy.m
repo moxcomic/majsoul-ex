68b3a11775a7cebf1d4738692f7b357d9112beb4
moxcomic <moxcomic@gmail.com>
2020-01-01 11:20:03 +0800

    update

--- Pods/Target Support Files/NSURLProtocolWebKitSupport/NSURLProtocolWebKitSupport-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_NSURLProtocolWebKitSupport : NSObject
-@end
-@implementation PodsDummy_NSURLProtocolWebKitSupport
-@end
--- Pods/Target Support Files/Pods-majsoulUITests/Pods-majsoulUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_majsoulUITests : NSObject
-@end
-@implementation PodsDummy_Pods_majsoulUITests
-@end
